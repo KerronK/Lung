58600597e6bcfce7bbad555b72aef87c13d73fcd
KerronK <122799774+KerronK@users.noreply.github.com>
2023-02-26 15:08:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+MODEL_FILE_PATH = r"C:\Users\kerro\Desktop\Lung_Deploy_new\Naive_Bayes_Model_new.pkl"
+
+JSON_FILE_PATH = r"C:\Users\kerro\Desktop\Lung_Deploy_new\project_data.json"
+#
+# MODEL_FILE_PATH = r"C:\Users\kerro\Desktop\Ankush_Sir\Pro1\KNN_Model.pkl"
+# #
+# JSON_FILE_PATH = r"C:\Users\kerro\Desktop\Ankush_Sir\Pro1\project_data_KNN.json"
